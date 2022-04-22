932215a518ece222b98d496c82c649e46b4a7e68
Temitayo Afolabi <linuxmathew1245@gmail.com>
2022-04-22 17:53:08 +0100

    First Commit

---
+++ src/Components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import axios from 'axios';
+import SearchInput from '../SearchInput';
+import ImageList from './ImageList';
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = { images: []}
+        this.onSearchSubmit = this.onSearchSubmit.bind(this)
+    }
+
+    async onSearchSubmit(entry){
+        const response = await axios.get(`https:/pixabay.com/api/?key=26756551-87eec8e23bb0177b70f40ace4&q=${entry}&image_type=photo`)
+        this.setState ({ images:response.data.hits })
+    }
+
+    render(){
+        return(
+            <div className='ui container' style={{marginTop: '30px'}}>
+                <SearchInput onSearchSubmit={this.onSearchSubmit} />
+                <ImageList images = { this.state.images } />
+            </div>
+        )
+    }
+    
+}
+
+
+export default App;
